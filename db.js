8ef1a784fd1fc7589d86e3b8467e37f6d3e4259c
Harshvardhan Singh Khurmi <hvskhurmi19@gmail.com>
2024-11-29 15:16:30 +0530

    Edit db.js for production

--- db.js
+++ db.js
@@ -7,26 +7,11 @@ dotenv.config();
 const { Pool } = pkg;
 
 const isProduction = process.env.NODE_ENV === 'production';
-
 const connectionString = process.env.DATABASE_URL;
-
-const ssl = isProduction
-  ? { rejectUnauthorized: false } // For Heroku
-  : false; // For local development
 
 const pool = new Pool({
   connectionString,
   ssl: isProduction ? { rejectUnauthorized: false } : undefined,
-});
-
-// Optional: Test the connection
-pool.connect((err, client, release) => {
-  if (err) {
-    console.error('Error acquiring client:', err.stack);
-  } else {
-    console.log('Connected to PostgreSQL database');
-    release();
-  }
 });
 
 export default pool;
